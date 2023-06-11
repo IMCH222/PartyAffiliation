fc2eea591661aee8927a4f2edc686061979915e6
IMCH222 <chukwuim@mail.uc.edu>
2023-06-11 11:22:14 -0400

    Task 3 finished

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class PartyAffiliation {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        String partyChoice = "";
+        String party = "D - Democrat\tR - Republican\tI - Independent\tO - other\nChoose your party: ";
+
+        System.out.print(party);
+        partyChoice = in.nextLine();
+
+        if(partyChoice.equalsIgnoreCase("D")) {
+            System.out.println("You get a Democratic Donkey!");
+        }
+        else if(partyChoice.equalsIgnoreCase("R")) {
+            System.out.println("You get a Republican Elephant!");
+        }
+        else if(partyChoice.equalsIgnoreCase("I")){
+            System.out.println("You get a Independent Eagle!");
+        }
+        else if(partyChoice.equalsIgnoreCase("O")) {
+            System.out.println("You get an American Flag!");
+        }
     }
 }
